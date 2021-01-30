19a9aa549bda3a0c29ee075dff030b5aea1c28ae
ramin <ramin@RAMIN-PC>
2021-01-30 22:14:51 +0330

    ..

--- Inventory-Asp-Core-MVC-Ajax/Businesses/Classes/DashboardBiz.cs
+++ Inventory-Asp-Core-MVC-Ajax/Businesses/Classes/DashboardBiz.cs
@@ -1,6 +1,5 @@
 ﻿using AspNetCore.Lib.Models;
 using AspNetCore.Lib.Services.Interfaces;
-using Inventory_Asp_Core_MVC_Ajax.Businesses.Common;
 using Inventory_Asp_Core_MVC_Ajax.Businesses.Interfaces;
 using Inventory_Asp_Core_MVC_Ajax.Core.Classes;
 using Inventory_Asp_Core_MVC_Ajax.DataAccess.EFModels;
@@ -11,27 +10,20 @@ namespace Inventory_Asp_Core_MVC_Ajax.Businesses.Classes
     public class DashboardBiz : IDashboardBiz
     {
         private readonly IRepository _repository;
-        private readonly ISeedData _seedData;
 
-        public DashboardBiz(IRepository repository, ISeedData seedData)
+        public DashboardBiz(IRepository repository)
         {
             _repository = repository;
-            _seedData = seedData;
         }
 
-        public Task<Result<DashboardModel>> Statistics() => Result<DashboardModel>.TryAsync(async () =>
-        {
-           // await _seedData.LoadSampleData();
-
-
-            return Result<DashboardModel>.Successful(new DashboardModel()
+        public Task<Result<DashboardModel>> Statistics() => Result<DashboardModel>
+            .TryAsync(async () => Result<DashboardModel>.Successful(new DashboardModel()
             {
                 TotalStorages = (await _repository.CountAllAsync<Storage>()).Data,
                 TotalSuppliers = (await _repository.CountAllAsync<Supplier>(s => s.Enabled)).Data,
                 TotalProducts = (await _repository.CountAllAsync<Product>()).Data
-            });
-        });
-
+            })
+        );
 
     }
 }
